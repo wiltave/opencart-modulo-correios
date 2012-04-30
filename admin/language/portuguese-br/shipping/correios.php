e030150d546c72c13979b1cfbde72452ebff6ff1
Willian Gustavo Veiga <wiltave@gmail.com>
2012-04-30 18:31:12 -0300

    Compatible with OpenCart 1.5.2.1

---
+++ admin/language/portuguese-br/shipping/correios.php
@@ -0,0 +1,35 @@
+<?php
+// Heading
+$_['heading_title']       = 'Correios (Brasil)';
+
+// Text
+$_['text_shipping']       = 'Entrega';
+$_['text_success']        = 'Módulo Correios atualizado com sucesso!';
+$_['text_sim']        	  = 'Sim';
+$_['text_nao']        	  = 'Não';
+$_['text_sedex']       	  = 'Sedex';
+$_['text_sedex_cobrar']   = 'Sedex a Cobrar';
+$_['text_pac']       	  = 'PAC';
+$_['text_sedex_10']       = 'Sedex 10';
+$_['text_esedex']         = 'e-Sedex';
+
+// Entry
+$_['entry_status']        = 'Situação:';
+$_['entry_tax']       	  = 'Classe de Impostos:';
+$_['entry_geo_zone']      = 'Área Geográfica:';
+$_['entry_postcode']      = 'CEP de origem:';
+$_['entry_sort_order']    = 'Ordenação:';
+$_['entry_mao_propria']   = 'Mão própria:<br /><span class="help">É o serviço opcional pelo qual o remetente recebe a garantia de que o objeto, por ele postado sob registro, será entregue somente ao próprio destinatário, através da confirmação de sua identidade.</span>';
+$_['entry_aviso_recebimento']   = 'Aviso de recebimento:<br /><span class="help">É o serviço adicional que, através do preenchimento de formulário próprio, físico ou digital, permite comprovar, junto ao remetente, a entrega do objeto.</span>';
+$_['entry_declarar_valor']		= 'Declarar valor:<br /><span class="help">É o serviço adicional pelo qual o cliente declara o valor de um objeto postado sob registro, para fins de ressarcimento, em caso de extravio ou espoliação. Mesmo desabilitado, será aplicado ao Sedex a Cobrar.</span>';
+$_['entry_adicional']   		= 'Valor adicional (%):<br /><span class="help">Adicional somado ao valor final do frete. Não aplicável ao Sedex a Cobrar.</span>';
+$_['entry_servicos']   			= 'Serviços:<br /><span class="help">Habilite os serviços que deseja usar em sua loja.</span>';
+$_['entry_prazo_adicional']   	= 'Prazo de entrega adicional (dias):<br /><span class="help">Prazo de entrega adicional somado ao dos Correios. Não aplicável ao Sedex 10.</span>';
+$_['entry_esedex']    = 'Acesso ao e-Sedex:<br/><span class="help">Código administrativo e a senha para acesso ao serviço.</span>';
+$_['entry_esedex_codigo']   = 'Código:';
+$_['entry_esedex_senha']    = 'Senha:';
+// Error
+$_['error_permission']    = 'Atenção: Você não tem permissão para alterar o módulo Correios!';
+$_['error_postcode']      = 'Formato inválido de CEP!';
+$_['error_servico']       = 'Pelo menos um serviço dos Correios deve estar habilitado!';
+?>
---
+++ catalog/language/portuguese-br/shipping/correios.php
@@ -0,0 +1,15 @@
+<?php
+$_['text_41106']  = 'PAC. Entrega: %s dias úteis.';
+$_['text_40010']  = 'Sedex. Entrega: %s dias úteis.';
+$_['text_40215']  = 'Sedex 10.';
+$_['text_40045']  = 'Sedex a Cobrar. Entrega: %s dias úteis. Valor retirada: %s.';
+$_['text_81019']  = 'e-Sedex. Entrega: %s dias úteis.';
+
+$_['text_title']  = 'Correios';
+
+$_['error_dim'] 			= 'Módulo Correios (Brasil) -> produto com dimensões ou peso inválidos ou não cadastrados: %s';
+$_['error_dim_limite'] 		= 'Módulo Correios (Brasil) -> produto com dimensões acima do permitido (%sx%sx%s, CxLxA em cm): %s (%sx%sx%s)';
+$_['error_dim_soma'] 		= 'Módulo Correios (Brasil) -> produto com a soma das dimensões acima do limite permitido (%scm): %s (%scm)';
+$_['error_peso'] 			= 'Módulo Correios (Brasil) -> produto com o peso acima do permitido (%sKg): %s (%sKg)';
+
+?>
